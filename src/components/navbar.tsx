93d1f9fe87968b89981b7969ddafe63c88805bcd
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-15 00:20:30 +0800

    Add icons and logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import Link from "next/link";
+import { RiTwitterXLine, RiGithubFill  } from "react-icons/ri";
 
 export default function Navbar() {
   return (
     <nav className='flex items-center justify-between p-2 h-[60px] border'>
-        <div>Sajal</div>
+        <div className="text-xl font-bold">Sajal</div>
         <div className='flex items-center justify-between gap-2'>
-            <div>Twitter</div>
-            <div>Github</div>
+            <Link target="_blink" href='https://x.com/ashsajal1'><RiTwitterXLine /></Link>
+            <Link target="_blink" href='https://github.com/ashsajal1'><RiGithubFill /></Link>
         </div>
     </nav>
   )
