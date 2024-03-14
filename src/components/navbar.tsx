5985394090913f3508d4a0380e2cb9720abc72ca
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-15 01:24:25 +0800

    Change color of navbar icons

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,8 +6,8 @@ export default function Navbar() {
     <nav className='flex bg-gradient-to-br from-primaryLow to-secondaryLow items-center justify-between py-2 px-12 h-[60px] border'>
         <div className="text-2xl font-bold gradient-text">Sajal</div>
         <div className='flex items-center justify-between gap-2'>
-            <Link target="_blink" href='https://x.com/ashsajal1'><RiTwitterXLine /></Link>
-            <Link target="_blink" href='https://github.com/ashsajal1'><RiGithubFill /></Link>
+            <Link target="_blink" href='https://x.com/ashsajal1'><RiTwitterXLine className="text-primary" /></Link>
+            <Link target="_blink" href='https://github.com/ashsajal1'><RiGithubFill className="text-primary h-5 w-5" /></Link>
         </div>
     </nav>
   )
