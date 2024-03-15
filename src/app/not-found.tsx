6605a4969c2d43274dfc68d28b6d64d7dfbe15b4
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-15 14:36:34 +0800

    Update not found image

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,16 @@
-export default function NotFound() {
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+export default function NotFoundPage() {
     return (
-        <h3>Not found</h3>
-    )
+        <div className="flex flex-col items-center justify-center h-screen">
+            <div className='w-[200px] h-[200px]'>
+                <Image className='object-cover' width={200} height={200} src="/not-found-image.png" alt="Not Found" />
+            </div>
+            <h1 className="text-4xl font-bold mb-4">404 - Not Found</h1>
+            <p className="text-lg text-gray-600 mb-8">Sorry, the page you are looking for does not exist.</p>
+            <Link href="/" className="text-blue-500 hover:underline">Go back to home</Link>
+        </div>
+    );
 }
