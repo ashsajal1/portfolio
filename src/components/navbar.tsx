c99ff98315d4d90de04044796858e1dc4648ce17
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-15 13:56:02 +0800

    Add href in logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ import SwitchButton from "./switch-button";
 export default function Navbar() {
   return (
     <nav className='flex bg-gradient-to-br from-primaryLow to-secondaryLow items-center justify-between py-2 px-12 h-[60px] border'>
-        <div className="text-2xl font-bold gradient-text">Sajal</div>
+        <Link href='/' className="text-2xl font-bold gradient-text">Sajal</Link>
         <div className='flex items-center justify-between gap-2'>
             <Link target="_blink" href='https://x.com/ashsajal1'><RiTwitterXLine className="text-primary" /></Link>
             <Link target="_blink" href='https://github.com/ashsajal1'><RiGithubFill className="text-primary h-5 w-5" /></Link>
