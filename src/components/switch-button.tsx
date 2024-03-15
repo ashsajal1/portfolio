f75b6614de46345d48a4bb3eee10a6884a7cd36a
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-15 12:50:20 +0800

    Add toggle theme functionality in switch button

--- src/components/switch-button.tsx
+++ src/components/switch-button.tsx
@@ -1,11 +1,11 @@
-"use client"
-import React, { useState } from 'react';
+'use client'
+import { useTheme } from "next-themes"
 
 const SwitchButton = () => {
-  const [isChecked, setIsChecked] = useState(false);
+  const { theme, setTheme } = useTheme();
 
   const handleToggle = () => {
-    setIsChecked(!isChecked);
+    setTheme(theme === 'light' ? 'dark' : 'light');
   };
 
   return (
@@ -14,15 +14,15 @@ const SwitchButton = () => {
         type="checkbox"
         id="toggle"
         className="hidden"
-        checked={isChecked}
+        checked={theme === 'dark'}
         onChange={handleToggle}
       />
       <label
         htmlFor="toggle"
         className="flex items-center cursor-pointer"
       >
-        <div className={`w-12 h-6 flex items-center bg-gray-300 rounded-full p-1 duration-300 ease-in-out ${isChecked ? 'bg-green-400' : 'bg-gray-400'}`}>
-          <div className={`bg-white w-5 h-5 rounded-full shadow-md transform duration-300 ease-in-out ${isChecked ? 'translate-x-5' : ''}`}></div>
+        <div className={`w-12 h-6 flex items-center dark:bg-gray-300 rounded-full p-1 duration-300 ease-in-out bg-green-400`}>
+          <div className={`bg-white w-5 h-5 rounded-full shadow-md transform duration-300 ease-in-out dark:translate-x-0  translate-x-5`}></div>
         </div>
         <span className="ml-3 text-gray-700">Toggle</span>
       </label>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from "tailwindcss";
-import { violet, green, blue } from "tailwindcss/colors";
+import { violet, blue } from "tailwindcss/colors";
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
