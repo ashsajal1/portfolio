00e2294ed5784591b3a98cd3692588d6bee5157f
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-15 13:26:03 +0800

    Add opacity

--- src/components/blob.tsx
+++ src/components/blob.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Blob() {
   return (
-    <div className='h-[100px] w-[100px] top-24 left-12 z-0 inset-0 absolute bg-gradient-to-br from-primary to-secondary dark:blur-xl mix-blend-multiply blur-3xl rounded-full'></div>
+    <div className='h-[100px] w-[100px] top-24 left-12 -z-12 inset-0 absolute bg-gradient-to-br from-primary to-secondary filter blur-2xl mix-blend-multiply rounded-full opacity-70'></div>
   )
 }
