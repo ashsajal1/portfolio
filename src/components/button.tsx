ba8f28f1c5dc9b0a9840e866aa776f451fa027e0
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-15 13:54:27 +0800

    set select none in button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react'
 
 export default function Button({ label, icon }: { label: string, icon?: ReactNode }) {
     return (
-        <div className='p-2 rounded-md text-slate-50 flex items-center justify-between gap-2 bg-gradient-to-br from-primary to-secondary hover:to-secondaryHigh transition-all duration-50 cursor-pointer'>
+        <div className='p-2 rounded-md select-none text-slate-50 flex items-center justify-between gap-2 bg-gradient-to-br from-primary to-secondary hover:to-secondaryHigh transition-all duration-50 cursor-pointer'>
             {icon && icon}
             <span>{label}</span>
         </div>
