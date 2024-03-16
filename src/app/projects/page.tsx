89a1d9664a6792e85542d1e58073fa4aa7a39781
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-16 12:17:29 +0800

    Design header

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,7 @@ import Project from "@/components/project";
 export default function Projects() {
   return (
     <div>
-      <h3>Here is a list of projects!</h3>
+      <h3 className="text-xl gradient-text font-medium text-center p-2 border-b w-1/2 m-auto border-secondary">Here is a list of projects!</h3>
 
       <div className="grid grid-cols-1 md:grid-cols-2 p-2 gap-2">
         <Project />
