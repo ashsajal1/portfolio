749fab464f335c9d98c442b60d15a236e86bafa3
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-16 11:11:37 +0800

    Decrease the width of first item in hero section

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image'
 export default function HeroSection() {
     return (
         <div className='flex flex-col sm:flex-row items-center relative p-12 justify-between w-full'>
-            <div className='order-2 sm:order-1'>
+            <div className='order-2 sm:order-1 w-1/2'>
                 <h3 className='text-3xl font-extrabold bg-gradient-to-br from-primary to-secondary bg-clip-text text-transparent'>Building Projects with React, TypeScript, and Next.js</h3>
                 <p className='py-2'>Exploring Advanced Technologies to Enhance and Simplify Everyday Life</p>
 
