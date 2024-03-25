f8d982499d598157e9adf4b84a087786e2866b07
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-25 14:02:35 +0800

    Render projects dynamically

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,5 @@
 import Project from "@/components/project";
+import { projects } from "@/lib/projectsList";
 
 export default function Projects() {
   return (
@@ -6,9 +7,9 @@ export default function Projects() {
       <h3 className="text-xl gradient-text font-medium text-center p-2 border-b w-1/2 m-auto border-secondary">Here is a list of projects!</h3>
 
       <div className="grid grid-cols-1 md:grid-cols-2 p-2 gap-2">
-        <Project title="AI Image Classification" image="/webapp/reactjs-nextjs-gig-image-1.png" description="Ai image classification built with Next.js and Typescript. API is provided by Huggingface" url="https://nextjs-image-classification.vercel.app/" repo='https://github.com/ashsajal1/nextjs-image-classification' />
-        <Project title="AI Image Classification" image="/webapp/reactjs-nextjs-gig-image-1.png" description="AI Image Classification" url="https://nextjs-image-classification.vercel.app/" repo='https://github.com/ashsajal1/nextjs-image-classification' />
-        <Project title="AI Image Classification" image="/webapp/reactjs-nextjs-gig-image-1.png" description="AI Image Classification" url="https://nextjs-image-classification.vercel.app/" repo='https://github.com/ashsajal1/nextjs-image-classification' />
+        {projects.map(({title, description, repo, url, image}) => (
+          <Project key={repo} title={title} description={description} repo={repo} url={url} image={image} />
+        ))}
         
       </div>
     </div>
--- src/lib/projectsList.ts
+++ src/lib/projectsList.ts
@@ -4,5 +4,15 @@ export const projects = [
     url: "https://nextjs-image-classification.vercel.app/",
     repo: "https://github.com/ashsajal1/nextjs-image-classification",
     image: "/webapp/reactjs-nextjs-gig-image-1.png",
+    description:
+      "This image classification AI app, developed with Next.js and Typescript, leverages an API provided by Huggingface. Through Next.js, it offers efficient rendering and server-side rendering capabilities for web applications. Typescript ensures robust type checking and enhanced code maintainability.",
+  },
+  {
+    title: "AI Question Answering",
+    url: "https://ai-qna-six.vercel.app/",
+    repo: "https://github.com/ashsajal1/ai-qna",
+    image: "/webapp/reactjs-nextjs-gig-image-1.png",
+    description:
+      "The AI web application is designed to respond to inquiries within a specific context, utilizing the API provided by Hugging Face. It has been developed using Next.js and Tailwind CSS.",
   },
 ];
