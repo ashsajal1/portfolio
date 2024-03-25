651c5ffd2bbf80c583003d5959be9df54d75fbf2
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-25 14:15:54 +0800

    Add projects in array

--- src/lib/projectsList.ts
+++ src/lib/projectsList.ts
@@ -11,8 +11,32 @@ export const projects = [
     title: "AI Question Answering",
     url: "https://ai-qna-six.vercel.app/",
     repo: "https://github.com/ashsajal1/ai-qna",
-    image: "/webapp/reactjs-nextjs-gig-image-1.png",
+    image: "/webapp/reactjs-nextjs-gig-image-2.png",
     description:
       "The AI web application is designed to respond to inquiries within a specific context, utilizing the API provided by Hugging Face. It has been developed using Next.js and Tailwind CSS.",
+  },
+  {
+    title: "Phowwcase : Showcase projects",
+    url: "https://phowwcase.vercel.app",
+    repo: "https://github.com/ashsajal1/ai-qna",
+    image: "/webapp/reactjs-nextjs-gig-image-3.png",
+    description:
+      "Presenting a sophisticated web application designed to showcase developers' most favored projects, complete with GitHub repository links and previews. This innovative platform offers a seamless user experience, allowing visitors to explore developers' noteworthy projects and preview their functionalities directly from the interface. With its integration of GitHub repositories and project previews, this web app serves as a valuable tool for developers seeking inspiration and collaboration opportunities.",
+  },
+  {
+    title: "AI Prompt Marketplace",
+    url: "https://ai-promptm.vercel.app/",
+    repo: "https://github.com/ashsajal1/ai-promptm",
+    image: "/webapp/reactjs-nextjs-gig-image-4.png",
+    description:
+      "In the ever-evolving landscape of artificial intelligence (AI), prompt-based models have gained significant prominence. These models allow developers to generate creative and contextually relevant text by providing a prompt. Imagine a marketplace where developers can explore, share, and utilize these powerful AI prompts seamlessly. Enter the AI Prompt Marketplace built with Angular.",
+  },
+  {
+    title: "AI Text Generation",
+    url: "https://nextjs-txt-gen.vercel.app/",
+    repo: "https://github.com/ashsajal1/nextjs-txt-gen",
+    image: "/webapp/reactjs-nextjs-gig-image-5.png",
+    description:
+      "This web application is built using Next.js and TypeScript. It leverages the Huggingface API for text generation. Users can input prompts, and the app generates creative and contextually relevant text based on the provided input.",
   },
 ];
