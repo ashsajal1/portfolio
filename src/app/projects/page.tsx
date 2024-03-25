dc784e20832cba9a51e4d93d7ba0ca02939cbb36
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-03-25 13:52:33 +0800

    Create project array

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -9,7 +9,6 @@ export default function Projects() {
         <Project title="AI Image Classification" image="/webapp/reactjs-nextjs-gig-image-1.png" description="Ai image classification built with Next.js and Typescript. API is provided by Huggingface" url="https://nextjs-image-classification.vercel.app/" repo='https://github.com/ashsajal1/nextjs-image-classification' />
         <Project title="AI Image Classification" image="/webapp/reactjs-nextjs-gig-image-1.png" description="AI Image Classification" url="https://nextjs-image-classification.vercel.app/" repo='https://github.com/ashsajal1/nextjs-image-classification' />
         <Project title="AI Image Classification" image="/webapp/reactjs-nextjs-gig-image-1.png" description="AI Image Classification" url="https://nextjs-image-classification.vercel.app/" repo='https://github.com/ashsajal1/nextjs-image-classification' />
-        <Project title="AI Image Classification" image="/webapp/reactjs-nextjs-gig-image-1.png" description="AI Image Classification" url="https://nextjs-image-classification.vercel.app/" repo='https://github.com/ashsajal1/nextjs-image-classification' />
         
       </div>
     </div>
---
+++ src/lib/projectsList.ts
@@ -0,0 +1,8 @@
+export const projects = [
+  {
+    title: "AI Image Classification",
+    url: "https://nextjs-image-classification.vercel.app/",
+    repo: "https://github.com/ashsajal1/nextjs-image-classification",
+    image: "/webapp/reactjs-nextjs-gig-image-1.png",
+  },
+];
