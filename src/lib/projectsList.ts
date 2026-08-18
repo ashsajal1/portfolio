35cb313972b381226d61df0449f6b22e26e1e7b5
Ashsajal <ashsajal1@gmail.com>
2026-08-18 19:17:48 +0600

    docs: update with live url

--- src/lib/projectsList.ts
+++ src/lib/projectsList.ts
@@ -1,77 +1,49 @@
 export const projects = [
   {
-    title: "AI Image Classification",
-    url: "https://nextjs-image-classification.vercel.app/",
-    repo: "https://github.com/ashsajal1/nextjs-image-classification",
-    image: "/webapp/reactjs-nextjs-gig-image-1.png",
-    description:
-      "This image classification AI app, developed with Next.js and Typescript, leverages an API provided by Huggingface. Through Next.js, it offers efficient rendering and server-side rendering capabilities for web applications. Typescript ensures robust type checking and enhanced code maintainability.",
-  },
-  {
-    title: "AI Question Answering",
-    url: "https://ai-qna-six.vercel.app/",
-    repo: "https://github.com/ashsajal1/ai-qna",
-    image: "/webapp/reactjs-nextjs-gig-image-2.png",
-    description:
-      "The AI web application is designed to respond to inquiries within a specific context, utilizing the API provided by Hugging Face. It has been developed using Next.js and Tailwind CSS.",
-  },
-  {
-    title: "Phowwcase : Showcase projects",
-    url: "https://phowwcase.vercel.app",
-    repo: "https://github.com/ashsajal1/phowwcase",
-    image: "/webapp/reactjs-nextjs-gig-image-3.png",
-    description:
-      "Presenting a sophisticated web application designed to showcase developers' most favored projects, complete with GitHub repository links and previews. This innovative platform offers a seamless user experience, allowing visitors to explore developers' noteworthy projects and preview their functionalities directly from the interface. With its integration of GitHub repositories and project previews, this web app serves as a valuable tool for developers seeking inspiration and collaboration opportunities.",
-  },
-  {
-    title: "AI Prompt Marketplace",
-    url: "https://ai-promptm.vercel.app/",
-    repo: "https://github.com/ashsajal1/ai-promptm",
-    image: "/webapp/reactjs-nextjs-gig-image-4.png",
-    description:
-      "In the ever-evolving landscape of artificial intelligence (AI), prompt-based models have gained significant prominence. These models allow developers to generate creative and contextually relevant text by providing a prompt. Imagine a marketplace where developers can explore, share, and utilize these powerful AI prompts seamlessly. Enter the AI Prompt Marketplace built with Angular.",
-  },
-  {
-    title: "AI Text Generation",
-    url: "https://nextjs-txt-gen.vercel.app/",
-    repo: "https://github.com/ashsajal1/nextjs-txt-gen",
-    image: "/webapp/reactjs-nextjs-gig-image-5.png",
-    description:
-      "This web application is built using Next.js and TypeScript. It leverages the Huggingface API for text generation. Users can input prompts, and the app generates creative and contextually relevant text based on the provided input.",
-  },
-  {
     title: "Typing App",
     repo: "https://github.com/ashsajal1/typing-app",
+    image: "/typing-app.png",
+    url: "https://typing-practice-pro.netlify.app/",
     description:
       "The Typing Practice App is a web application built with React.js that allows users to practice and improve their typing skills with a simple and intuitive interface.",
   },
   {
-    title: "Next SaaS Template",
-    repo: "https://github.com/ashsajal1/next-saas-template",
-    description:
-      "A comprehensive starter kit for building scalable SaaS applications with Next.js, featuring built-in authentication, database integration, and responsive design.",
-  },
-  {
     title: "Mini Course",
     repo: "https://github.com/ashsajal1/mini-course",
+    image: "/mini-course.png",
+    url: "https://minicourse.netlify.app/",
     description:
       "Empowering learners with quality education and practical skills for the modern world. Built with Next.js and PostgreSQL.",
   },
   {
     title: "Sohojboi",
     repo: "https://github.com/ashsajal1/sohojboi",
+    image: "/sohojboi.png",
+    url: "https://sohojboi.netlify.app/",
     description:
       "An interactive platform to ask and answer questions, participate in quizzes, and access educational content through a blog.",
   },
   {
     title: "JS Practice App",
     repo: "https://github.com/ashsajal1/js-practice-app",
+    image: "/js-practice-app.png",
+    url: "https://js-practices-app.netlify.app/",
     description:
       "An app to practice programming related topics and concepts — JavaScript, Rust, Golang, React and more.",
   },
   {
+    title: "Next SaaS Template",
+    repo: "https://github.com/ashsajal1/next-saas-template",
+    image: "/next-saas-template.png",
+    url: "https://next-saas.netlify.app/",
+    description:
+      "A comprehensive starter kit for building scalable SaaS applications with Next.js, featuring built-in authentication, database integration, and responsive design.",
+  },
+  {
     title: "Calorie Tracker",
     repo: "https://github.com/ashsajal1/calorie-tracker",
+    image: "/calorie-tracker.png",
+    url: "https://calorie-trackers-pro.netlify.app/",
     description:
       "A sleek and responsive calorie tracker app built with Vue.js 3, TypeScript, and Tailwind CSS. Log meals, track daily intake, and monitor fitness goals in real time.",
   },
